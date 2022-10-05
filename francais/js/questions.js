58400e35385d82baa548bff5bb67c0256cf4fe35
Fraançois Bib <91905543+FrancoisBib@users.noreply.github.com>
2022-10-05 15:21:29 +0100

    Add files via upload

---
+++ francais/js/questions.js
@@ -0,0 +1,72 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "C'est quoi solar parker probe?",
+    answer: "est un observatoire solaire spatial développé parla NASA ",
+    options: [
+      "est un observatoire solaire spatial développé par la NASA",
+      "c'est un projet de la NASA",
+      "C'est une mission de sauvetage",
+      "C'est un departement "
+    ]
+  },
+    {
+    numb: 2,
+    question: "Quel est l'année de lancement de la sonde parker?",
+    answer: "2018",
+    options: [
+      "1950",
+      "2010",
+      "2018",
+      "2020"
+    ]
+  },
+    {
+    numb: 3,
+    question: "Qu est Eugène Parker?",
+    answer: "est un astrophysicien américain spécialiste du Soleil.",
+    options: [
+      "Le chef du projet solar probe parker",
+      "Un astrophysicien américain spécialiste du Soleil",
+      "Celui qui à decouvert la planète Mars",
+      "Un chercheur"
+    ]
+  },
+    {
+    numb: 4,
+    question: "Quel était le but de la mission solar parker probe?",
+    answer: "explorer les mystères du Soleil en voyageant plus près de lui que n’importe quel vaisseau spatial auparavant",
+    options: [
+      "Visitez la lune ",
+      "Vistez Mars",
+      "Allez chercher des aliènnes",
+      "explorer les mystères du Soleil en voyageant plus près de lui que n’importe quel vaisseau spatial auparavant"
+    ]
+  },
+    {
+    numb: 5,
+    question: "Combien de planète existe t-il ?",
+    answer: "9",
+    options: [
+      "8",
+      "9",
+      "6",
+      "1"
+    ]
+  },
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+  //   {
+  //   numb: 6,
+  //   question: "Your Question is Here",
+  //   answer: "Correct answer of the question is here",
+  //   options: [
+  //     "Option 1",
+  //     "option 2",
+  //     "option 3",
+  //     "option 4"
+  //   ]
+  // },
+];
---
+++ main.js
@@ -0,0 +1,21 @@
+$(function() {
+
+  $('.custom-dropdown').on('show.bs.dropdown', function() {
+    // $(this).find('.dropdown-menu').first().stop(false, false).slideDown();
+     // $(this).find('.dropdown-menu').addClass('active');
+     var that = $(this);
+    setTimeout(function(){
+        that.find('.dropdown-menu').addClass('active');
+    }, 100);
+
+
+  });
+  $('.custom-dropdown').on('hide.bs.dropdown', function() {
+    $(this).find('.dropdown-menu').removeClass('active');
+  });
+
+});
+
+$('.dropdown-link').mouseenter(function(){
+  $('.dropdown-menu').fadeIn(1000);
+});
